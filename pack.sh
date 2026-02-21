ffa11f2b25ff55f3d29ddc5b09745eb68ff7939f
Haochen Sun <haochen.sun@uwaterloo.ca>
2026-02-21 15:09:13 -0500

    Update MMCleaner - 2026-02-21 15:09:13

--- pack.sh
+++ pack.sh
@@ -4,4 +4,4 @@ git add .
 # Get current date and time for commit message
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 git commit -m "Update MMCleaner - $current_time"
-git push origin main
+git push origin master
